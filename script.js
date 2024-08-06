a350d07a8ac4496cc5da971d407bdce2fe88c0d9
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2024-08-06 19:38:38 +0300

    simple weather forecast chrome extension for learning chrome extension developing

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tempField = document.getElementById("temp");
+    const weatherForecastField = document.getElementById("weather-forecast");
+    const weatherIconImg = document.getElementById("weather-icon");
+    const selectElement = document.getElementById('cities-menu');
+    const getForecastButton = document.getElementById('get-forecast-btn');
+
+    const apiKey = 'cd45c0c21800a75b4f46ed9c7efb6c3a';
+
+    const getForecast = () => {
+        const city = selectElement.value;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const weatherDescription = data["weather"][0]['description'];
+                const temperature = data["main"]['temp'];
+                const iconCode = data["weather"][0]["icon"];
+                const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+                tempField.innerHTML = `${temperature}°C`;
+                weatherForecastField.innerHTML = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
+                weatherIconImg.style.display = "block";
+                weatherIconImg.src = iconUrl;
+                weatherIconImg.alt = weatherDescription;
+            })
+            .catch(error => {
+                console.error('API çağrısında hata:', error);
+            });
+    }
+
+    getForecastButton.addEventListener('click', getForecast);
+});
