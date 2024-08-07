5d3aca199ee3865e45c88721364b43774ade4dda
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2024-08-07 13:50:46 +0300

    change select element to text input

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const tempField = document.getElementById("temp");
     const weatherForecastField = document.getElementById("weather-forecast");
     const weatherIconImg = document.getElementById("weather-icon");
-    const selectElement = document.getElementById('cities-menu');
+    // const selectElement = document.getElementById('cities-menu');ü
+    const cityInputField = document.getElementById("city-text-input");
     const getForecastButton = document.getElementById('get-forecast-btn');
+    const errorMessageField = document.getElementById("error-message");
+
 
     const apiKey = 'cd45c0c21800a75b4f46ed9c7efb6c3a';
+    // inputtan al 
 
     const getForecast = () => {
-        const city = selectElement.value;
+        const city = cityInputField.value;
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
         fetch(url)
             .then(response => response.json())
             .then(data => {
+                console.log(data);
+                errorMessageField.style.display = "none";
+                if (data["message"] == "city not found") {
+                    errorMessageField.style.display = "block";
+                }
                 const weatherDescription = data["weather"][0]['description'];
                 const temperature = data["main"]['temp'];
                 const iconCode = data["weather"][0]["icon"];
