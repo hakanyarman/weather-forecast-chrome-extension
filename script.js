cf4ff6ffdeb6c25bea98ce65e2de3771dd2dd4a3
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2024-08-07 14:13:47 +0300

    add enter keypress event listener

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const weatherBox = document.getElementById("weather-box");
     const tempField = document.getElementById("temp");
     const weatherForecastField = document.getElementById("weather-forecast");
     const weatherIconImg = document.getElementById("weather-icon");
-    // const selectElement = document.getElementById('cities-menu');ü
     const cityInputField = document.getElementById("city-text-input");
     const getForecastButton = document.getElementById('get-forecast-btn');
     const errorMessageField = document.getElementById("error-message");
 
-
     const apiKey = 'cd45c0c21800a75b4f46ed9c7efb6c3a';
-    // inputtan al 
 
     const getForecast = () => {
         const city = cityInputField.value;
@@ -18,21 +16,24 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
-                errorMessageField.style.display = "none";
                 if (data["message"] == "city not found") {
                     errorMessageField.style.display = "block";
-                }
-                const weatherDescription = data["weather"][0]['description'];
-                const temperature = data["main"]['temp'];
-                const iconCode = data["weather"][0]["icon"];
-                const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+                    weatherBox.style.display = "none";  // Hide weather box when city not found
+                } else {
+                    errorMessageField.style.display = "none"; // Hide error message when city is found
+                    weatherBox.style.display = "block"; // Show weather box when city is found
 
-                tempField.innerHTML = `${temperature}°C`;
-                weatherForecastField.innerHTML = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
-                weatherIconImg.style.display = "block";
-                weatherIconImg.src = iconUrl;
-                weatherIconImg.alt = weatherDescription;
+                    const weatherDescription = data["weather"][0]['description'];
+                    const temperature = data["main"]['temp'];
+                    const iconCode = data["weather"][0]["icon"];
+                    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+                    tempField.innerHTML = `${temperature}°C`;
+                    weatherForecastField.innerHTML = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
+                    weatherIconImg.style.display = "block";
+                    weatherIconImg.src = iconUrl;
+                    weatherIconImg.alt = weatherDescription;
+                }
             })
             .catch(error => {
                 console.error('API çağrısında hata:', error);
@@ -40,4 +41,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     getForecastButton.addEventListener('click', getForecast);
+    document.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+            getForecast();
+        }
+    });
+
 });
