e5cb460f43cd7d2c7cc2947e64b1fd18c1ea44f9
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2024-08-10 00:25:16 +0300

    add city name not entered error ,humidity,feels like,wind values. and beautify the style

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const cityInputField = document.getElementById("city-text-input");
     const getForecastButton = document.getElementById('get-forecast-btn');
     const errorMessageField = document.getElementById("error-message");
+    const humidityField = document.getElementById("hum");
+    const feelsLikeField = document.getElementById("feels-like");
+    const windField = document.getElementById("wind");
 
     const apiKey = 'cd45c0c21800a75b4f46ed9c7efb6c3a';
 
@@ -16,23 +19,39 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch(url)
             .then(response => response.json())
             .then(data => {
+                console.log(data);
                 if (data["message"] == "city not found") {
                     errorMessageField.style.display = "block";
-                    weatherBox.style.display = "none";  // Hide weather box when city not found
-                } else {
-                    errorMessageField.style.display = "none"; // Hide error message when city is found
-                    weatherBox.style.display = "block"; // Show weather box when city is found
+                    weatherBox.style.display = "none";
+                    errorMessageField.innerHTML = "City not found";
+
+                } else if (city == "") {
+                    errorMessageField.innerHTML = "Please enter a city name";
+                    errorMessageField.style.display = "block";
+                    weatherBox.style.display = "none";
+                }
+                else {
+                    errorMessageField.innerHTML = "";
+
+                    errorMessageField.style.display = "none";
+                    weatherBox.style.display = "block";
 
                     const weatherDescription = data["weather"][0]['description'];
                     const temperature = data["main"]['temp'];
                     const iconCode = data["weather"][0]["icon"];
                     const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+                    const humidity = data["main"]["humidity"];
+                    const feelsLike = data["main"]["feels_like"];
+                    const wind = data["wind"]["speed"];
 
                     tempField.innerHTML = `${temperature}°C`;
                     weatherForecastField.innerHTML = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
                     weatherIconImg.style.display = "block";
                     weatherIconImg.src = iconUrl;
                     weatherIconImg.alt = weatherDescription;
+                    humidityField.innerHTML = `Humidity: ${humidity}%`;
+                    feelsLikeField.innerHTML = `Feels Like: ${feelsLike}°C`;
+                    windField.innerHTML = `Wind: ${wind} m/s`;
                 }
             })
             .catch(error => {
